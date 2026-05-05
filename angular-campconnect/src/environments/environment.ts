1afc18797c0ecb62a4d17f68530618122c9a26b8
Tlili Houssein <houssien.tlili@esprit.tn>
2026-05-05 05:00:40 +0100

    Update API URL to production environment

--- angular-campconnect/src/environments/environment.ts
+++ angular-campconnect/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8090/api",
+  apiUrl: "https://campconnect-api-gybaebesg3hgahcu.swedencentral-01.azurewebsites.net/api",
   stripePublicKey: "pk_test_51SfPS8LJGJeTxiWFoIowATeRU1xC9UtWQYrykg0sthPpI1mD8cRv8KiCGZvjIo7BCAYPM4MGiPM3zzNRyZVqjs4C006QhqTGtx"
 };
